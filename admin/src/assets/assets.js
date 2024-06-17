319b2ac8c9d32a78921fb6fcc7786080cecf77b8
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2024-06-18 00:29:44 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://food-del-backend-2rh6.onrender.com'
 export const currency = '₹'
 
 export const assets ={
