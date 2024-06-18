ac20d03f852966b1d525d33cd821df06a91389af
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2024-06-18 20:47:49 +0530

    Update ExploreMenu.jsx

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -8,8 +8,8 @@ const ExploreMenu = ({category,setCategory}) => {
   
   return (
     <div className='explore-menu' id='explore-menu'>
-      <h1>Explore our menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <h1>Explore our products</h1>
+      <p className='explore-menu-text'>Explore our diverse collection of exquisite beauty products. Our mission is to fulfill your beauty needs and elevate your self-care routine, one luxurious product at a time..</p>
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return (
