629090e16bd42f28bf9cf30c7ca0777c5040f870
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2024-06-18 20:55:10 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -28,12 +28,12 @@ const Footer = () => {
             <h2>GET IN TOUCH</h2>
             <ul>
                 <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>contact@glamlane.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2024 © Glam-Lane.com - All Right Reserved.</p>
     </div>
   )
 }
