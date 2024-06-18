3cf4db496df1a0665eff1be8733d01a3c2829f65
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2024-06-18 09:30:02 +0530

    Update foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -28,7 +28,7 @@ const addFood = async (req, res) => {
         })
 
         await food.save();
-        res.json({ success: true, message: "Food Added" })
+        res.json({ success: true, message: "Product Added" })
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Error" })
@@ -43,7 +43,7 @@ const removeFood = async (req, res) => {
         fs.unlink(`uploads/${food.image}`, () => { })
 
         await foodModel.findByIdAndDelete(req.body.id)
-        res.json({ success: true, message: "Food Removed" })
+        res.json({ success: true, message: "Product Removed" })
 
     } catch (error) {
         console.log(error);
