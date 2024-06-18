076de3f0b6459e7b1b9082da02870a38e1436274
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2024-06-18 09:51:56 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='header-contents'>
-                <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <h2>Discover Your Beauty Heaven</h2>
+                <p>Shop now and discover the secret to radiant skin. With our range of skincare products, you can unlock the confidence that comes with a flawless complexion!</p>
                 <button>View Menu</button>
             </div>
         </div>
