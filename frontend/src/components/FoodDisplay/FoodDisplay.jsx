090af631e8c5e54e11d5c2d55c43357ff768488b
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2024-06-18 20:49:08 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,7 +9,7 @@ const FoodDisplay = ({category}) => {
 
   return (
     <div className='food-display' id='food-display'>
-      <h2>Top dishes near you</h2>
+      <h2>Varieties of products for you.</h2>
       <div className='food-display-list'>
         {food_list.map((item)=>{
           if (category==="All" || category===item.category) {
